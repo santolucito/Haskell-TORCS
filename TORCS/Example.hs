2f23db1671d436d85e1c877724bd0f85e54ede21
Mark Santolucito <mark.santolucito@yale.edu>
2017-02-06 15:21:15 -0500

    autocommit

--- TORCS/Example.hs
+++ TORCS/Example.hs
@@ -13,19 +13,28 @@ import Debug.Trace
 soloDrive :: IO ()
 soloDrive = startDriver myDriver
 
-twoPlayer = startDrivers [myDriver,myDriver]
+twoPlayer = startDrivers [myDriver,myDriver2]
+
+myDriver2 :: Driver
+myDriver2 = proc e -> do
+    CarState{..} <- arr getE -< e
+    rec
+       g <- arr shifting -< (rpm,gear')
+       s <- arr (steering 0) -< (angle,trackPos)
+       a <- arr (gas 210) -< (speedX,s)
+       msg <- arr (\g->if g>4 then "im fast" else "im starting") -< g
+    returnA -< defaultDriveState {accel = a, gear = g, steer = s, broadcast = msg}
 
 myDriver :: Driver
 myDriver = proc e -> do
     CarState{..} <- arr getE -< e
     rec
        g <- arr shifting -< (rpm,gear')
-       s <- arr steering -< (angle,trackPos)
-       a <- arr gas -< (speedX,s)
+       s <- arr (steering 0.1) -< (angle,trackPos)
+       a <- arr (gas 210) -< (speedX,s)
        msg <- arr (\g->if g>4 then "im fast" else "im starting") -< g
     returnA -< defaultDriveState {accel = a, gear = g, steer = s, broadcast = msg}
 
-targetSpeed = 210
 
 shifting :: (Double,Int) -> Int
 shifting (rpm,g) = if 
@@ -33,16 +42,16 @@ shifting (rpm,g) = if
   | rpm < 3000 -> max 1 (g-1)
   | otherwise  -> g
  
-steering :: (Double,Double) -> Double
-steering (angle,trackPos) = let
+steering :: Double -> (Double,Double) -> Double
+steering offset (angle,trackPos) = let
   turns = angle*14 / pi
-  centering = turns - ((trackPos)*0.2)
+  centering = turns - ((trackPos+offset)*0.2)
   clip x = max (-1) $ min x 1
  in
   clip centering
   
-gas :: (Double,Double) -> Double
-gas (speed,steer) = 
+gas :: Double -> (Double,Double) -> Double
+gas targetSpeed (speed,steer) = 
   if speed < (targetSpeed-((abs steer)*1800)) then 0.8 else 0
   
 getE :: Event CarState -> CarState
