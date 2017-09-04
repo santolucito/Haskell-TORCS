bc805c47c57e97592c3813d23a17ea569e7c1c1c
Mark Santolucito <mark.santolucito@yale.edu>
2017-09-04 01:10:55 -0400

    move SGD to its own place

--- TORCS/Examples/Learning/Optimizing.hs
+++ TORCS/Examples/Learning/Optimizing.hs
@@ -11,6 +11,7 @@ import TORCS.Types
 import TORCS.Connect
 import Control.Concurrent
 import TORCS.Examples.Learning.Simple
+import TORCS.Examples.Learning.SGD
 import Debug.Trace
 
 import System.Process
@@ -23,47 +24,10 @@ import Control.Lens
 
 optimize :: Thetas
 optimize = 
-  multiVarGD 
-    0.1 0.001 
+  multiVarSGD 
+    [speed, turning]
+    2 0.1 0.001 
     (Thetas {_speed=160.0,_turning=14}) (Thetas {_speed=0,_turning=0}) evalDriver
-
-batchSize = 2
-allLenses = [speed, turning]
-
--- | Use data type Theta for params to be passed to driver
-multiVarGD :: Double -> Double -> Thetas -> Thetas -> (Thetas -> IO Double) -> Thetas
-multiVarGD goal learnRate t t_prev f =
-  let
-    --TODO better convergance fxn
-    converged = (abs((_speed t)-(_speed t_prev)) <= goal)
-    continueGD = multiVarGD goal learnRate updatedThetas t f
-    -- NB use a fixed theta for takeStep, but update a seperate copy of theta in the fold
-    updatedThetas = foldr (takeStep learnRate t f) t (stochastic allLenses)
-  in
-    if not converged
-    then continueGD
-    else t
-
-stochastic :: [a] -> [a]
-stochastic xs =
-  take batchSize $ shuffle' xs (length xs) rand
- where
-  --TODO get rid of unsafe here, pass in the gen from the top io level and there is no problem
-  rand = unsafePerformIO newStdGen
-
-
--- TODO get rid of unsafe here? (actually its fine here, but would be a nice exercise to fix)
-takeStep :: Double -> Thetas -> (Thetas -> IO Double) -> _Lens -> Thetas -> Thetas
-takeStep learnRate t f part updatedTheta = 
-  over part (\x -> x - learnRate * (unsafePerformIO $ partialDerivative f part t)) updatedTheta
-
--- TODO there must be a better way
--- but for now just move in one direction and interpolate 
-partialDerivative :: (Thetas -> IO Double) -> _Lens -> Thetas -> IO Double
-partialDerivative f part t = do
-  x1 <- f t
-  x2 <- f (over part (\x->x*1.01) t)
-  return $ ((x2 - x1) / 0.01)
 
 evalDriver :: Thetas -> IO Double
 evalDriver t@Thetas{..} = do
