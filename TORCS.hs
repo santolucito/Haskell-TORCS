45ec36dc8ffdb35442bfb895bd9faeaa911c8757
Mark Santolucito <mark.santolucito@yale.edu>
2018-01-06 19:58:33 +0900

    remve neural stuff

--- TORCS.hs
+++ TORCS.hs
@@ -3,11 +3,9 @@ module TORCS where
 
 import TORCS.Types
 import TORCS.Connect
-import TORCS.ConnectVerbose
 --import TORCS.Example
 import TORCS.Examples.SimpleExample
 import TORCS.Examples.ComplexExample
 --import TORCS.Examples.Learning.Optimizing
 import TORCS.Examples.MultiExample
-import TORCS.Examples.NN.NN2
 
